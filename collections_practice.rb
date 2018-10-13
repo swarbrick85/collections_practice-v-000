13f0e688b34df97999cd16c5fbaddcdb85e89ec4
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-13 11:24:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
+def sort_array_asc(ints)
+  return ints.sort 
+end 
 
+def sort_array_desc(ints)
+  return ints.sort.reverse 
+end 
+
+def sort_array_char_count(strings)
+  strings.sort do |a, b| 
+    a.length <=> b.length  
+  end 
+  return strings 
+end 
