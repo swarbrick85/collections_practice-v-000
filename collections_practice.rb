557c47787b4af1429a925275a6c1056f3cf1ee53
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-13 11:28:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,6 +10,10 @@ def sort_array_char_count(strings)
   strings = strings.sort_by {|x| x.length}
 end 
 
-def swap_elements 
-  
+def swap_elements(array)
+  a = array[1]
+  b = array[2]
+  array[1] = b 
+  array[2] = a 
+  return array
 end 
